ccf9d9c7ea08a4ed99949a542fd25e3588fb47d2
tuserl <testuserlmaolol@gmail.com>
2025-06-19 07:04:11 +0700

    funfacts script updated for grep: (standard input): binary file matches

--- mysh/funfacts/funfacts.sh
+++ mysh/funfacts/funfacts.sh
@@ -9,7 +9,8 @@ touch "$FACT_FILE"
 TODAY=$(date +%Y-%m-%d)
 
 # Check if any facts already fetched today
-FACTS_TODAY=$(grep "^$TODAY|" "$FACT_FILE" | cut -d'|' -f2-)
+#FACTS_TODAY=$(grep "^$TODAY|" "$FACT_FILE" | cut -d'|' -f2-)
+FACTS_TODAY=$(grep --text "^$TODAY|" "$FACT_FILE" | cut -d'|' -f2-)
 #FACTS_TODAY=$(cut -d'|' -f2- "$FACT_FILE")
 
 if [ -n "$FACTS_TODAY" ]; then
@@ -22,22 +23,29 @@ fi
 while true; do
   #  FACT=$(curl -s "https://www.randomfunfacts.com/" | grep "<strong>" | sed -E "s/^.*<i>([^<]*)<\/i>.*$/\1/" | xargs)
   # Fixed xargs: unmatched single quote; by default quotes are special to xargs unless you use the -0 option
+  #FACT=$(curl -s "https://www.randomfunfacts.com/" |
+  #  grep "<strong>" |
+  #  sed -E "s/^.*<i>([^<]*)<\/i>.*$/\1/" |
+  #  sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
   FACT=$(curl -s "https://www.randomfunfacts.com/" |
-    grep "<strong>" |
-    sed -E "s/^.*<i>([^<]*)<\/i>.*$/\1/" |
+    grep -oP '(?<=<i>).*?(?=</i>)' |
+    head -n1 |
     sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+
+  # Optional debug:
+  echo "      FACT=$FACT"
   if [ -n "$FACT" ]; then
     # Check if this exact fact already saved today
     #    if grep -qxF "$TODAY|$FACT" "$FACT_FILE"; then
     # Check if this exact fact already saved all time
     if grep -qF "|$FACT" "$FACT_FILE"; then
-      echo "Duplicate fact, skipping..."
+      echo "⚠️ Duplicate fact, skipping..."
     else
       echo "$TODAY|$FACT" >>"$FACT_FILE"
-      echo "Added: $FACT"
+      echo "✅ Added: $FACT"
     fi
   else
-    echo "Empty result, retrying..."
+    echo "🛑 Empty result, retrying..."
   fi
   sleep 1
 done
